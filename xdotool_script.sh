b39b233616eeb5a7d60b5dd33a5e832a84a51ef7
moqsien <moqsien@foxmail.com>
2025-03-21 22:14:47 +0800

    xdotool

--- xdotool_script.sh
+++ xdotool_script.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+# for alacritty
 ACTIVE=$(xdotool getactivewindow)
 WIN_ID=$(xdotool search --class AlacrittyDropdown)
 
@@ -9,6 +10,25 @@ alias startAtty="alacritty --class AlacrittyDropdown"
 if [ -z "$WIN_ID" ]; then
     # 启动 Alacritty
     startAtty  &
+elif [[ "$WIN_ID" == "$ACTIVE" ]]; then
+    xdotool windowunmap $WIN_ID
+else
+    # 切换可见性
+    xdotool windowmap $WIN_ID
+fi
+
+# for kitty
+KITTY_CLASS="kittydropdown"
+
+ACTIVE=$(xdotool getactivewindow)
+WIN_ID=$(xdotool search --class kittydropdown)
+
+
+alias startKtty="kitty --class $KITTY_CLASS --title KittyTerminal"
+
+if [ -z "$WIN_ID" ]; then
+    # 启动 Kitty
+    startKtty  &
 elif [[ "$WIN_ID" == "$ACTIVE" ]]; then
     xdotool windowunmap $WIN_ID
 else
